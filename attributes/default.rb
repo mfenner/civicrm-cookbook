c95f3106c3d2ef56614979e84fd2ae7df719ab85
Martin Fenner <mfenner@plos.org>
2013-10-26 15:08:24 +0200

    change Apache docroot

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,6 @@ default[:civicrm][:version] = "4.4.0"
 default[:civicrm][:download_file] = "civicrm-#{node[:civicrm][:version]}-wordpress.zip"
 default[:civicrm][:download_url] = "http://sourceforge.net/projects/civicrm/files/civicrm-stable/#{node[:civicrm][:version]}/#{node[:civicrm][:download_file]}"
 default[:apache][:default_site_enabled] = true
-default[:apache][:docroot_dir] = "/vagrant"
 default[:apache][:user] = "vagrant"
 default[:apache][:group] = "vagrant"
 default[:mysql][:server_root_password] = SecureRandom.hex(8)
